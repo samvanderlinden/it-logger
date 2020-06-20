675f3ee4d2818dd28cbb22751e0f565555f3855e
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-06-19 23:22:45 -0500

    add update log functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from './types';
 
 // export const getLogs = () => {
 //     //redux thunk allows us to return a function in our actions rather than just returning an object with a type and payload
@@ -84,6 +84,49 @@ export const deleteLog = (id) => async (dispatch) => {
     }
 
 };
+
+//Update log
+export const updateLog = (log) => async (dispatch) => {
+    try {
+        setLoading();
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await res.json();
+
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        });
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        });
+    }
+
+};
+
+
+//Set current log
+export const setCurrent = (log) => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+}
+
+//Clear current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT
+    }
+}
 
 //Set loading to true
 export const setLoading = () => {
